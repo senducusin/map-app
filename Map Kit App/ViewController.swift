448e82535e3ee5643889d26929695e6a564803d0
Jansen Ducusin <senducusin@gmail.com>
2021-04-10 10:03:40 +0800

    Initial commit

---
+++ Map Kit App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Map Kit App
+//
+//  Created by Jansen Ducusin on 4/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+    }
+
+
+}
+
